63379bc477ce6dd3298969a444347458a5446899
Daniil Evstigneev <officialevsty@192.168.0.7>
2025-07-03 01:22:09 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OfficialEvsty/aa-shared
+
+go 1.24.3
---
+++ models/parsed_models.go
@@ -0,0 +1,33 @@
+package models
+
+// Item json struct holds data
+type Item struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Tier     int    `json:"tier"`
+	TierURL  string `json:"tierUrl"`
+	ImageURL string `json:"image_url"`
+}
+
+// DropItem json struct holds data of boss's drop items
+type DropItem struct {
+	Item     Item   `json:"item"`
+	Quantity string `json:"quantity"`
+}
+
+// Boss struct represents parsed data from AA db site about boss and his loot
+type Boss struct {
+	ID   int         `json:"boss_id"`
+	Name string      `json:"boss_name"`
+	Loot []*DropItem `json:"loot"`
+}
+
+// Server parsed data
+type Server struct {
+	ID int `json:"server_id"`
+}
+
+// Unit data about each server
+type Unit struct {
+	ID int `json:"unit_id"`
+}
